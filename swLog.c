eb1ddee60dc162deb8bacb00fcf6288fbb829b9f
Yi-Tung <d.seeker.1995@gmail.com>
2025-05-12 13:59:13 +0800

    For 'swLog' module, change some mutex locks to readers-writer locks for efficiency

--- swLog.c
+++ swLog.c
@@ -12,10 +12,10 @@
 #define SWLOG_LOCK_FILE_NAME "./swLog.lock"
 
 
-static pthread_mutex_t g_log_level_lock = PTHREAD_MUTEX_INITIALIZER;
+static pthread_rwlock_t g_log_level_lock = PTHREAD_RWLOCK_INITIALIZER;
 static swLog_level_t g_log_level = SWLOG_LEVEL_HIDE;
 
-static pthread_mutex_t g_swLog_file_lock = PTHREAD_MUTEX_INITIALIZER;
+static pthread_rwlock_t g_swLog_file_name_lock = PTHREAD_RWLOCK_INITIALIZER;
 static char g_swLog_file_name[512] = "./swLog.log";
 static _Atomic int g_swLog_store_switch = 0;
 
@@ -27,9 +27,9 @@ static void _store_swLog(const char *mMsg) {
   static pthread_mutex_t inner_lock = PTHREAD_MUTEX_INITIALIZER;
   char log_file_name[512] = {0};
 
-  pthread_mutex_lock(&g_swLog_file_lock);
+  pthread_rwlock_rdlock(&g_swLog_file_name_lock);
   snprintf(log_file_name, sizeof(log_file_name), "%s", g_swLog_file_name);
-  pthread_mutex_unlock(&g_swLog_file_lock);
+  pthread_rwlock_unlock(&g_swLog_file_name_lock);
 
   if(!access(log_file_name, F_OK | W_OK)) {
     int lock_fd = open(SWLOG_LOCK_FILE_NAME, O_CREAT | O_RDWR | O_CLOEXEC, S_IWUSR | S_IRUSR | S_IWGRP | S_IRGRP);
@@ -70,9 +70,9 @@ void pr_swLog(swLog_level_t mLevel, char *mMsg, ...) {
   static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
   swLog_level_t log_level;
 
-  pthread_mutex_lock(&g_log_level_lock);
+  pthread_rwlock_rdlock(&g_log_level_lock);
   log_level = g_log_level;
-  pthread_mutex_unlock(&g_log_level_lock);
+  pthread_rwlock_unlock(&g_log_level_lock);
 
   if( (log_level & mLevel) && (mMsg != NULL) ) {
     int output_fd = atomic_load(&g_swLog_output_fd);
@@ -83,12 +83,14 @@ void pr_swLog(swLog_level_t mLevel, char *mMsg, ...) {
     va_list ap;
 
     va_start(ap, mMsg);
+    vsnprintf(msg_buf, sizeof(msg_buf), mMsg, ap);
+    va_end(ap);
+
     if(localtime_r(&log_time, &log_time_info) == NULL) {
       fprintf(stderr, "%s[%d]: Failed to translate local time", __func__, __LINE__);
-      va_end(ap);
       return;
     }
-    vsnprintf(msg_buf, sizeof(msg_buf), mMsg, ap);
+
     snprintf(log_buf, sizeof(log_buf), "[%04d-%02d-%02d %02d:%02d:%02d] %s\n"
       , log_time_info.tm_year + 1900
       , log_time_info.tm_mon  + 1
@@ -97,7 +99,6 @@ void pr_swLog(swLog_level_t mLevel, char *mMsg, ...) {
       , log_time_info.tm_min
       , log_time_info.tm_sec
       , msg_buf);
-    va_end(ap);
 
     if(atomic_load(&g_swLog_pr_switch)) {
       pthread_mutex_lock(&lock);
@@ -117,9 +118,9 @@ void set_swLog_level(swLog_level_t mLevel) {
     case SWLOG_LEVEL_ERROR:
     case SWLOG_LEVEL_WARNING:
     case SWLOG_LEVEL_INFO:
-      pthread_mutex_lock(&g_log_level_lock);
+      pthread_rwlock_wrlock(&g_log_level_lock);
       g_log_level = mLevel;
-      pthread_mutex_unlock(&g_log_level_lock);
+      pthread_rwlock_unlock(&g_log_level_lock);
       break;
     default:
       break;
@@ -129,15 +130,15 @@ void set_swLog_level(swLog_level_t mLevel) {
 swLog_level_t get_swLog_level(void) {
   swLog_level_t log_level;
 
-  pthread_mutex_lock(&g_log_level_lock);
+  pthread_rwlock_rdlock(&g_log_level_lock);
   log_level = g_log_level;
-  pthread_mutex_unlock(&g_log_level_lock);
+  pthread_rwlock_unlock(&g_log_level_lock);
 
   return log_level;
 }
 
 void set_swLog_file_name(char *mName, size_t mSize) {
-  pthread_mutex_lock(&g_swLog_file_lock);
+  pthread_rwlock_wrlock(&g_swLog_file_name_lock);
 
   static const size_t len = sizeof(g_swLog_file_name);
   if(mName != NULL && mSize > 1) {
@@ -146,11 +147,11 @@ void set_swLog_file_name(char *mName, size_t mSize) {
     }
   }
 
-  pthread_mutex_unlock(&g_swLog_file_lock);
+  pthread_rwlock_unlock(&g_swLog_file_name_lock);
 }
 
 int get_swLog_file_name(char *mName, size_t mSize) {
-  pthread_mutex_lock(&g_swLog_file_lock);
+  pthread_rwlock_rdlock(&g_swLog_file_name_lock);
 
   size_t len = strlen(g_swLog_file_name) + 1;
   int ret = 0;
@@ -159,7 +160,7 @@ int get_swLog_file_name(char *mName, size_t mSize) {
     ret = 1;
   }
 
-  pthread_mutex_unlock(&g_swLog_file_lock);
+  pthread_rwlock_unlock(&g_swLog_file_name_lock);
   return ret;
 }
 

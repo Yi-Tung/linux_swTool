e77256d680d5ff1f44146582512ad167a9e2e15d
Yi-Tung <d.seeker.1995@gmail.com>
2025-06-17 23:32:49 +0800

    Add a function to read the IPv4 address of a network interface in 'swNetwork' module

--- modules/swNetwork/swNetwork.c
+++ modules/swNetwork/swNetwork.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h>
+#include <arpa/inet.h>
 #include <net/if.h>
 
 
@@ -93,6 +94,61 @@ int get_swNetwork_mac_addr(const char *mName, uint8_t *mMac) {
       && (strcmp(iface_addr_ptr->ifa_name, mName) == 0) ) {
       sa_dl = (struct sockaddr_dl*)(iface_addr_ptr->ifa_addr);
       memcpy(mMac, LLADDR(sa_dl), 6);
+      break;
+    }
+  }
+
+  freeifaddrs(iface_addr_head);
+  return 0;
+
+#else
+  return -1;
+
+#endif
+}
+
+int get_swNetwork_ipv4_addr(const char *mName, char *mIp, size_t mSize) {
+  if( (mName == NULL) || (mIp == NULL) || (mSize < 8) ) {
+    return -1;
+  }
+
+#if defined(__linux__)
+  struct sockaddr_in *sa_in;
+  struct ifreq iface_req;
+  int fd;
+
+  strncpy(iface_req.ifr_name, mName, IFNAMSIZ - 1);
+
+  fd = socket(AF_INET, SOCK_DGRAM, 0);
+  if(fd == -1) {
+    return -1;
+  }
+
+  if(ioctl(fd, SIOCGIFADDR, &iface_req) == -1) {
+    close(fd);
+    return -1;
+  }
+  else {
+    close(fd);
+  }
+
+  sa_in = (struct sockaddr_in*)iface_req.ifr_addr;
+  inet_ntop(AF_INET, &(sa_in->sin_addr), mIp, mSize);
+  return 0;
+
+#elif defined(__APPLE__)
+  struct ifaddrs *iface_addr_head, *iface_addr_ptr;
+  struct sockaddr_in *sa_in;
+
+  if(getifaddrs(&iface_addr_head) == -1) {
+    return -1;
+  }
+
+  for(iface_addr_ptr = iface_addr_head; iface_addr_ptr != NULL; iface_addr_ptr = iface_addr_ptr->ifa_next) {
+    if( (iface_addr_ptr->ifa_addr->sa_family  == AF_INET)
+      && (strcmp(iface_addr_ptr->ifa_name, mName) == 0) ) {
+      sa_in = (struct sockaddr_in*)(iface_addr_ptr->ifa_addr);
+      inet_ntop(AF_INET, &(sa_in->sin_addr), mIp, mSize);
       break;
     }
   }
--- modules/swNetwork/swNetwork.h
+++ modules/swNetwork/swNetwork.h
@@ -7,4 +7,6 @@ int swNetwork_is_iface_up(const char*);
 
 int get_swNetwork_mac_addr(const char*, uint8_t*);
 
+int get_swNetwork_ipv4_addr(const char*, char*, size_t);
+
 #endif

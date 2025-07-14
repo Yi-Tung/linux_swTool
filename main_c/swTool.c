962914d21e202cea89925e0ce3961814890004df
Yi-Tung <d.seeker.1995@gmail.com>
2025-07-15 06:52:32 +0800

    Optimize the code in ‘swTool’

--- main_c/swTool.c
+++ main_c/swTool.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
 #include <unistd.h>
 #include <libgen.h>
 #include <string.h>
@@ -21,17 +20,23 @@
 #define SWTOOL_LOG_FILE_NAME "./swTool.log"
 
 
+typedef struct cmd_args {
+  int argc;
+  void **argv;
+} cmd_args_t;
+
 typedef struct proc_info {
   pid_t exec_pid;
   char *exec_name;
   char *exec_pwd;
 
-  int (*action) (const char*, const int, ...);
+  int (*action) (const char*, const cmd_args_t);
   uint8_t property_flag;
+  cmd_args_t action_args;
 } proc_info_t;
 
 
-int show_usage(const char *tool_name, const int argc, ...) {
+int show_usage(const char *tool_name, const cmd_args_t args) {
   printf("Usage: %s [option]\n%s\n"
          , tool_name
          , "\noption:\n"
@@ -43,24 +48,24 @@ int show_usage(const char *tool_name, const int argc, ...) {
   return 0;
 }
 
-int show_version(const char *tool_name, const int argc, ...) {
+int show_version(const char *tool_name, const cmd_args_t args) {
   printf("%s: %s\n", tool_name, SWTOOL_VERSION);
   return 0;
 }
 
-int show_network_info(const char *tool_name, const int argc, ...) {
+int show_network_info(const char *tool_name, const cmd_args_t args) {
   char *iface_name = NULL;
   uint8_t mac[6] = {0};
   char ipv4[16] = {0};
   int status;
-  va_list ap;
+  int ret = 0;
 
-  va_start(ap, argc);
-  iface_name = va_arg(ap, char*);
+  iface_name = args.argv[0];
 
   status = swNetwork_is_iface_up(iface_name);
   if(status == -1) {
     printf("[%s]: some error occurred to get the network status\n", tool_name);
+    ret = -1;
   }
   else if(status == 0) {
     printf("[%s]: '%s' network interface is down\n", tool_name, iface_name);
@@ -73,6 +78,7 @@ int show_network_info(const char *tool_name, const int argc, ...) {
     }
     else {
       printf("[%s]: some errors occurred to get the mac address\n", tool_name);
+      ret = -1;
     }
 
     if(get_swNetwork_ipv4_addr(iface_name, ipv4, sizeof(ipv4)) == 0) {
@@ -80,11 +86,11 @@ int show_network_info(const char *tool_name, const int argc, ...) {
     }
     else {
       printf("[%s]: some errors occurred to get the IPv4 address\n", tool_name);
+      ret = -1;
     }
   }
 
-  va_end(ap);
-  return 0;
+  return ret;
 }
 
 int main(int argc, char *argv[]) {
@@ -96,14 +102,16 @@ int main(int argc, char *argv[]) {
   char *tool_name = basename(cp_argv0);
 
   proc_info_t *my_proc_info = malloc(sizeof(proc_info_t));
-  char *iface_name = NULL;
   char buf[512] = {0};
+  int index;
 
 
   my_proc_info->exec_pid = getpid();
   my_proc_info->exec_name = strdup(tool_name);
   my_proc_info->exec_pwd = getcwd(NULL, 0);
   my_proc_info->property_flag = 0;
+  my_proc_info->action_args.argc = 0;
+  my_proc_info->action_args.argv = NULL;
 
   free(cp_argv0);
   cp_argv0 = NULL;
@@ -142,10 +150,14 @@ int main(int argc, char *argv[]) {
       case 'h':
         my_proc_info->property_flag |= PROPERTY_VALID;
         my_proc_info->action = show_usage;
+        my_proc_info->action_args.argc = 0;
+        my_proc_info->action_args.argv = NULL;
         break;
       case 'v':
         my_proc_info->property_flag |= PROPERTY_VALID;
         my_proc_info->action = show_version;
+        my_proc_info->action_args.argc = 0;
+        my_proc_info->action_args.argv = NULL;
         break;
       case 'p':
         if(!strcmp(optarg, SWTOOL_MAGIC_NUM)) {
@@ -169,7 +181,9 @@ int main(int argc, char *argv[]) {
       case 'n':
         my_proc_info->property_flag |= PROPERTY_VALID;
         my_proc_info->action = show_network_info;
-        iface_name = strdup(optarg);
+        my_proc_info->action_args.argc = 1;
+        my_proc_info->action_args.argv = malloc(my_proc_info->action_args.argc * sizeof(void*));
+        my_proc_info->action_args.argv[0] = strdup(optarg);
         break;
       case '?':
       default:
@@ -193,21 +207,19 @@ int main(int argc, char *argv[]) {
       }
     }
 
-    if(my_proc_info->action == NULL) {
-
-    }
-    else if( (my_proc_info->action == show_usage)
-        || (my_proc_info->action == show_version) ) {
-      (my_proc_info->action)(my_proc_info->exec_name, 0);
-    }
-    else if(my_proc_info->action == show_network_info) {
-      (my_proc_info->action)(my_proc_info->exec_name, 1, iface_name);
+    if(my_proc_info->action != NULL) {
+      (my_proc_info->action)(my_proc_info->exec_name, my_proc_info->action_args);
     }
   }
 
 
-  if(iface_name != NULL) {
-    free(iface_name);
+  if(my_proc_info->action_args.argv != NULL) {
+    for(index=0; index<my_proc_info->action_args.argc; index++) {
+      if(my_proc_info->action_args.argv[index] != NULL) {
+        free(my_proc_info->action_args.argv[index]);
+      }
+    }
+    free(my_proc_info->action_args.argv);
   }
   free(my_proc_info->exec_name);
   free(my_proc_info->exec_pwd);
